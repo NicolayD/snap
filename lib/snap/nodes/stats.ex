af5b4bf348803dd1205e29f8adcd1b5bbb2013a8
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2024-09-08 16:46:17 +0300

    Fix Nodes.Stats filters parsing

--- lib/snap/nodes/stats.ex
+++ lib/snap/nodes/stats.ex
@@ -12,23 +12,24 @@ defmodule Snap.Nodes.Stats do
   @spec stats(module()) :: Cluster.result()
   @spec stats(module(), Keyword.t()) :: Cluster.result()
   def stats(cluster, filters \\ [], opts \\ []) do
-    nodes = parse_nodes(filters)
-    stats = parse_stats(filters)
-    index_metrics = parse_index_metrics(filters)
+    nodes = "/_nodes/" <> Keyword.get(filters, :nodes, "")
+    stats = "/stats/" <> Keyword.get(filters, :stats, "")
+    index_metrics = Keyword.get(filters, :index_metrics, "")
 
     url = nodes <> stats <> index_metrics
     Snap.get(cluster, url, [], [], opts)
   end
 
-  defp parse_nodes([nodes: nodes]) when is_binary(nodes), do: "/_nodes/#{nodes}/"
+  defp parse_nodes(nodes: nodes) when is_binary(nodes), do: "/_nodes/#{nodes}/"
 
   defp parse_nodes(_filters), do: "/_nodes/"
 
-  defp parse_stats([stats: stats]) when is_binary(stats), do: "stats/#{stats}/"
+  defp parse_stats(stats: stats) when is_binary(stats), do: "stats/#{stats}/"
 
   defp parse_stats(_filters), do: "stats/"
 
-  defp parse_index_metrics([index_metrics: index_metrics]) when is_binary(index_metrics), do: index_metrics
+  defp parse_index_metrics(index_metrics: index_metrics) when is_binary(index_metrics),
+    do: index_metrics
 
   defp parse_index_metrics(_filters), do: nil
 end
